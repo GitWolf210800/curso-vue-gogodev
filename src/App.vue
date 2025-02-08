b6102962a9177597e51d7835c1300f4d6fb866e2
GitWolf210800 <ezad821q@hotmail.com>
2025-02-08 19:43:04 -0300

    se agrega y prueba componente incrementar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
-  <h1>Listado de posts</h1>
-  <PostContainer />
+  <CounterOption />
 </template>
 
 <script lang="ts" setup>
-import PostContainer from './components/PostContainer.vue';
+import CounterOption from './components/CounterOption.vue';
 </script>
 
 <style>
-
+h1 {
+  color: brown;
+}
 </style>
