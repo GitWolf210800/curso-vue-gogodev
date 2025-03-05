d28a1789e612f5cf68890461ec6318459d022f48
GitWolf210800 <ezad821q@hotmail.com>
2025-03-05 07:20:50 -0300

    v-on composition API

--- src/components/ClassBind.vue
+++ src/components/ClassBind.vue
@@ -1,9 +1,13 @@
 <template>
-<p v-bind:class="{'danger' : hasError}">El mensaje se ha enviado satisfactoriamente</p>
+<p v-bind:style="{backgroundColor : '#ccc', color : c}">El mensaje se ha enviado satisfactoriamente</p>
+<button @click="handleClick()">Cambiar Color</button>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
 let hasError = true;
+let c = ref('#fff');
+const handleClick = () => c.value = '#ff0000';
 </script>
 
 <style scoped>
