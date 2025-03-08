1066e7ffecdd67b5133331df8915c3b34e40cef7
GitWolf210800 <ezad821q@hotmail.com>
2025-03-08 18:47:22 -0300

    modelForm composition API

--- src/components/ModelForm.vue
+++ src/components/ModelForm.vue
@@ -2,26 +2,21 @@
     <form>
         <input type="text" v-model="inputText" />
         <input type="checkbox" v-model="agree" /> Acepto los términos y condiciones
-        <select>
-            <option>Spain</option>
-            <option>UK</option>
-            <option>France</option>
+        <select v-model="country">
+            <option value="Spain">Spain</option>
+            <option value="Uk">UK</option>
+            <option value="France">France</option>
         </select>
     </form>
     <h3>{{ inputText }}</h3>
 </template>
 
-<script lang="ts">
-import { Ref, ref, defineComponent } from 'vue';
+<script lang="ts" setup>
+import { Ref, ref} from 'vue';
 
-export default defineComponent({
-    data() {
-        return {
-            inputText: '',
-            agree: true
-        }
-    }
-}); 
+let inputText = ref('');
+let agree = ref(true);
+let country = ref('Spain');
 
 </script>
 
